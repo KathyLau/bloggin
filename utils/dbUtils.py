f32575fb5ff60aee129f363b35cdc904c207a29d
williamx <wxiang@stuy.edu>
2016-11-02 22:40:44 -0400

    setup done

--- utils/dbUtils.py
+++ utils/dbUtils.py
@@ -1,30 +1,40 @@
 import sqlite3
-c = sqlite3.connect('data/tabular.db')
+c = sqlite3.connect('tabular.db')
 
 def setup():
     q = '''
     CREATE TABLE user (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INTEGER PRIMARY KEY,
     username VARCHAR(50),
-    password VARCHAR(50),
+    password VARCHAR(50)
     )
     '''
-    
+    c.execute(q)
+
     q = '''
     CREATE TABLE story (
-    id INT NOT NULL AUTO_INCREMENT,
-    int user_id int not null,
+    id INT PRIMARY KEY,
+    user_id INT NOT NULL,
     title TEXT,
     subtitle TEXT,
+    preContent TEXT,
     create_ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
     )
+    '''
+    c.execute(q)
 
+    q = '''
+    CREATE TABLE extension (
+    id INT PRIMARY KEY,
+    user_id INT NOT NULL,
+    story_id INT NOT NULL,
+    extContent TEXT,
+    create_ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+    )
     '''
     c.execute(q)
 
 
+setup()
 
-
-
-    
 c.close();
