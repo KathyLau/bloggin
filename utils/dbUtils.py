bbb6e4f2cea130a7659fde5c68a7f217a22df17b
williamx <wxiang@stuy.edu>
2016-11-02 23:50:08 -0400

    gitignore and dbUtils.login()

--- utils/dbUtils.py
+++ utils/dbUtils.py
@@ -1,11 +1,12 @@
 import sqlite3
-c = sqlite3.connect('tabular.db')
+conn = sqlite3.connect('tabular.db')
+c = conn.cursor()
 
 def setup():
     q = '''
     CREATE TABLE user (
     id INTEGER PRIMARY KEY,
-    username VARCHAR(50),
+    username VARCHAR(50) UNIQUE,
     password VARCHAR(50)
     )
     '''
@@ -15,7 +16,7 @@ def setup():
     CREATE TABLE story (
     id INT PRIMARY KEY,
     user_id INT NOT NULL,
-    title TEXT,
+    title TEXT UNIQUE,
     subtitle TEXT,
     preContent TEXT,
     create_ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
@@ -35,6 +36,41 @@ def setup():
     c.execute(q)
 
 
-setup()
+'''
+LOGIN: verifies login auth with db and returns corresponding int
+> Input: STRING username, STRING password
+> Output:
+  > 0 if all ok
+  > 1 if username doesn't exist
+  > 2 if username DOES exist but wrong password
+'''
+def login(username, password):
+    q = "SELECT 1 FROM user WHERE username=\"%s\" LIMIT 1;" % username
+    userExists = c.execute(q).fetchone()
 
-c.close();
+    if userExists:
+        q = "SELECT 1 FROM user WHERE username=\"%s\" AND password=\"%s\" LIMIT 1;" % (username, password)
+        correctPass = c.execute(q).fetchone()
+
+        if correctPass:
+            return 0
+        return 2
+    return 1
+
+
+def tmp():
+    q = "INSERT INTO user(username, password) VALUES(\"top\",\"kek\")"
+    c.execute(q);
+    q = "INSERT INTO user(username, password) VALUES(\"cop\",\"tech\")"
+    c.execute(q);
+
+
+def debug():
+    print login("cop","lek") #should be 2
+    print login("shop","kek") #should be 1
+    print login("top","kek") #should be 0
+    print login("cop","tech") #should be 0
+
+debug()
+conn.commit()
+conn.close()
