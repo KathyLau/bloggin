5d51695aad65abd33df29e4d98dcad128b42ab18
Kelly Wang <kwang6@stuy.edu>
2016-11-04 12:55:48 -0400

    imported session

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 import utils.dbUtils, hashlib 
 
 app = Flask(__name__)
