e28704933045a402f06dd0ccc14eb3467a151e3e
Daniel Monteagudo <dmonte3210@gmail.com>
2016-11-04 17:58:10 -0400

    Actually put links in the menu

--- static/js/mobile.js
+++ static/js/mobile.js
@@ -4,7 +4,7 @@ function checkMobile() {
 	navmenu = document.getElementById("navmenu")
 	navmenu.innerHTML = '<li class="menu"><a class="fa-bars" href="#menu">Menu</a></li>'
 	menu = document.getElementById("menudiv");
-	menu.innerHTML = '<section id="menu"><section><ul class="links"><li><a href="#"><h3>Lorem ipsum</h3><p>Feugiat tempus veroeros dolor</p></a></li><li><a href="#"><h3>Dolor sit amet</h3><p>Sed vitae justo condimentum</p></a></li><li><a href="#"><h3>Feugiat veroeros</h3><p>Phasellus sed ultricies mi congue</p></a></li><li><a href="#"><h3>Etiam sed consequat</h3><p>Porta lectus amet ultricies</p></a></li></ul></section></section>'
+	menu.innerHTML = '<section id="menu"><section><ul class="links"><li><a href="/"><h3>Your Stories</h3><p>View all the stories you\'ve contributed to</p></a></li><li><a href="/find"><h3>Find Stories</h3><p>Find and contribute to new stories</p></a></li><li><a href="/create"><h3>Create Story/h3><p>Start your own story</p></a></li></ul></section></section>'
     }
 }
 
